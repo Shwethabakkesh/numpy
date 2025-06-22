24f438213539332688ceee06467cb056e90f712f
Shwetha <shwethabakkesh03@gmail.com>
2025-06-22 15:47:46 +0530

    Add NumPy arithmetic operations

---
+++ operation.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#Addition and subtraction on array 
+a = np.array([[1,2],[2,1]])
+
+print("Array a: \n",a)
+print("Array a+1:\n",a+1)
+print("Attay a-1:\n",a-1)
+
+#adding two array
+b = np.array([[2,1],[1,2]])
+print("Array b:\n",b)
+print("Array a+b:\n",a+b)
+print("Array a+b:\n",a-b)
+print("\n__________________________________________________\n")
